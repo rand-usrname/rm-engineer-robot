7267121b7ad2b4b56600c28a05fe13f26c4a456b
yi_lu <1784570878@qq.com>
2020-08-14 22:58:27 +0800

    Update drv_gyro.c

--- public/drv_gyro.c
+++ public/drv_gyro.c
@@ -3,27 +3,25 @@
 //#include <arm_math.h>
 //#include "ahrs.h"
 
-struct IMU_t HERO_IMU;//IMU????,??m/s^2,rad/s
+struct IMU_t HERO_IMU;//IMU´«»ØÊý¾Ý,µ¥Î»m/s^2,rad/s
 struct IMU_atti_t HERO_IMU_atti;
-
-
 
 float invSqrt(float x);
 
 /***
   * @Name     gyro_read_angle
-  * @brief    ????????,CAN ID:101
-  * @param    rxmsg: [??] ,????CAN??
+  * @brief    ÍÓÂÝÒÇ½Ç¶ÈÖ¡¶ÁÈ¡,CAN ID:101
+  * @param    rxmsg: [ÊäÈë] ,½ÓÊÕµ½µÄCAN±¨ÎÄ
   * @retval   
   * @author   yi_lu
   * @Date     2020-08-11
 ***/
 void gyro_read_angle(struct rt_can_msg* rxmsg)
 {
-	HERO_IMU.atti_ready = rxmsg->data[7];		//????????
+	HERO_IMU.atti_ready = rxmsg->data[7];		//×ËÌ¬Êý¾ÝÕý³£±êÖ¾
 
-	HERO_IMU.pitch  = ((rt_int16_t)(rxmsg->data[0]<<8 | rxmsg->data[1])) / 100.0f;// pitch    -180----180,????,??:°
-	HERO_IMU.yaw 	= ((rt_int16_t)(rxmsg->data[2]<<8 | rxmsg->data[3])) / 100.0f;// yaw      -180----180,????
+	HERO_IMU.pitch  = ((rt_int16_t)(rxmsg->data[0]<<8 | rxmsg->data[1])) / 100.0f;// pitch    -180----180,ÉÏÕýÏÂ¸º,µ¥Î»:¡ã
+	HERO_IMU.yaw 	= ((rt_int16_t)(rxmsg->data[2]<<8 | rxmsg->data[3])) / 100.0f;// yaw      -180----180,ÓÒÕý×ó¸º
 	HERO_IMU.roll 	= ((rt_int16_t)(rxmsg->data[4]<<8 | rxmsg->data[5])) / 100.0f;// roll     -90----90,
 
 }
@@ -32,26 +30,26 @@ void gyro_read_angle(struct rt_can_msg* rxmsg)
 
 /***
   * @Name     gyro_read_speed
-  * @brief    ?????????,CAN ID:102
-  * @param    rxmsg: [??] ,????CAN??
+  * @brief    ÍÓÂÝÒÇ½ÇËÙ¶ÈÖ¡¶ÁÈ¡,CAN ID:102
+  * @param    rxmsg: [ÊäÈë] ,½ÓÊÕµ½µÄCAN±¨ÎÄ
   * @retval   
   * @author   yi_lu
   * @Date     2020-08-11
 ***/
 void gyro_read_speed(struct rt_can_msg* rxmsg)
 {
-	HERO_IMU.speed_ready = rxmsg->data[7];		//?????????
+	HERO_IMU.speed_ready = rxmsg->data[7];		//½ÇËÙ¶ÈÊý¾ÝÕý³£±êÖ¾
 
-	HERO_IMU.pitch_speed	= ((rt_int16_t)(rxmsg->data[0]<<8 | rxmsg->data[1])) / 100.0f;//??:°/s,1rpm=6°/s
+	HERO_IMU.pitch_speed	= ((rt_int16_t)(rxmsg->data[0]<<8 | rxmsg->data[1])) / 100.0f;//µ¥Î»:¡ã/s,1rpm=6¡ã/s
 	HERO_IMU.roll_speed 	= ((rt_int16_t)(rxmsg->data[2]<<8 | rxmsg->data[3])) / 100.0f;
 	HERO_IMU.yaw_speed 		= ((rt_int16_t)(rxmsg->data[4]<<8 | rxmsg->data[5])) / 100.0f;
 	
 }
 
-//??????????
+//Èç¹ûÔÆÌ¨¿ØÖÆÏß³Ì´æÔÚ
 #ifdef THREAD_GIMBAL_CONTROL
 
-//???(IMU)???????
+//´óµØÏµ(IMU)×ªµ½ÔÆÌ¨µç»úÏµ
 void IMU_transfer2_gimbal()
 {
 	gimbal_atti.pitch = HERO_IMU.pitch - ;
@@ -67,7 +65,7 @@ void IMU_transfer2_gimbal()
 	Gryo_data.yaw_speed 	= -sqrtf(Gryo_data.y_speed*Gryo_data.y_speed + Gryo_data.z_speed*Gryo_data.z_speed);
 	Gryo_data.pitch_speed 	= Gryo_data.x_speed;
 
-	if(Gryo_data.z_speed > Gryo_data.y_speed)//???????????
+	if(Gryo_data.z_speed > Gryo_data.y_speed)//·ûºÅÈ¡¾öÓÚ½Ï´óÖµµÄ·ûºÅ
 	{
 		if(Gryo_data.z_speed > 0)
 			Gryo_data.yaw_speed = -fabsf(Gryo_data.yaw_speed);
@@ -78,10 +76,12 @@ void IMU_transfer2_gimbal()
 #endif
 
 
+
 // Fast inverse square-root
 // See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
 
-float invSqrt(float x) {
+float invSqrt(float x) 
+{
 	float halfx = 0.5f * x;
 	float y = x;
 	long i = *(long*)&y;
