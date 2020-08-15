3d2af3359d51ba3f4dfc594b6e487e4e41eb8b09
2084046485 <2084046485@qq.com>
2020-08-15 11:04:07 +0800

    now remote and strike is useful

--- public/drv_remote.h
+++ public/drv_remote.h
@@ -1,74 +1,76 @@
 #ifndef __DRV_REMOTE_H__
 #define __DRV_REMOTE_H__
-
 #include <rtthread.h>
 #include <rtdevice.h>
 #include <board.h>
-
-typedef struct{
-	rt_uint16_t ch0;
-	rt_uint16_t ch1;
-	rt_uint16_t ch2;
-	rt_uint16_t ch3;
-	rt_uint8_t s1;
-	rt_uint8_t s2;
-
-} Remote_t;
-
-typedef struct{
-	rt_int16_t x_speed; //鼠标X轴速度
-	rt_int16_t y_speed; //鼠标Y轴速度
-	rt_int16_t z_speed; //暂时无用
-	rt_int8_t press_l;	//鼠标左键
-	rt_int8_t press_r; //鼠标右键
-
-} Mouse_t;
-
-typedef struct{
-	rt_uint8_t W;
-	rt_uint8_t S;
-	rt_uint8_t A;
-	rt_uint8_t D;
-	rt_uint8_t Q;
-	rt_uint8_t E;
-	rt_uint8_t shift;
-	rt_uint8_t ctrl;
-	rt_uint8_t R;
-	rt_uint8_t F;
-	rt_uint8_t G;
-	rt_uint8_t Z;
-	rt_uint8_t X;
-	rt_uint8_t C;
-	rt_uint8_t V;
-	rt_uint8_t B;
-
-} Key_Data_t;
-
+typedef enum
+{
+	no_action          =  0x00,
+	up_to_middle       =  0x01,
+	down_to_middle     =  0x02,
+	middle_to_up       =  0x03,
+	middle_to_down     =  0x04,
+	PRESS_ACTION       =  0x05,
+	LOOSEN_ACTION      =  0x06,
+	
+	
+	S1             =  0x07,
+	S2             =  0x08,
+	
+}switch_action_e;
+typedef struct  __Remote
+{
+	uint16_t ch0;
+	uint16_t ch1;
+	uint16_t ch2;
+	uint16_t ch3;
+	uint8_t s1;
+	uint8_t s2;
+}Remote_t;
+typedef struct  __Mouse
+{
+	int16_t x_speed;//向左负
+	int16_t y_speed;//向下正
+	int16_t z_speed;
+	int8_t press_l;
+	int8_t press_r;
+}Mouse_t;
+typedef	struct __Key_Data
+{
+	  uint8_t W;
+    uint8_t S;
+		uint8_t A;
+		uint8_t D;
+		uint8_t Q;
+		uint8_t E;
+		uint8_t shift;
+		uint8_t ctrl;
+	    uint8_t R;
+        uint8_t F;
+		uint8_t G;
+		uint8_t Z;
+		uint8_t X;
+		uint8_t C;
+		uint8_t V;
+		uint8_t B;
+}Key_Data_t;
+typedef struct __KeyBoard
+{
+	uint16_t v;
+	Key_Data_t Key_Data;
+}KeyBoard_t;
 typedef struct __RC_Ctrl
 {
-	Remote_t 	Remote_Data;	//遥控器部分数据
-	Mouse_t 	Mouse_Data;		//鼠标部分数据
-	Key_Data_t 	Key_Data;		//键盘部分数据
-} RC_Ctrl_t;
+	Remote_t Remote_Data;
+	Mouse_t Mouse_Data;
+	KeyBoard_t KeyBoard_Data;
+}RC_Ctrl_t;
 
-extern RC_Ctrl_t *RC_data;
+extern RC_Ctrl_t RC_Ctrl_Data;
+
 extern int remote_uart_init(void);
 
-
-rt_uint8_t Obtain_Sx_Data(rt_uint8_t num);
-rt_uint8_t Obtain_Key_Press(rt_uint8_t *targetdata);
-//使用时将要比较的数据传入函数，
-//if(RCD_change(RC_data->Key_Data.A))
-//即可判断键盘数据A是否改变
-//#define RCD_change(targetdata)	_RCD_change((rt_uint8_t*)(&targetdata),sizeof(targetdata))
-//extern rt_uint8_t _RCD_change(rt_uint8_t *targetdata,rt_uint8_t length);
-
-////判断某个键是否产生按下动作（！只能判断按键和鼠标数据，若传入其他参数会产生未知结果！）
-//#define RCD_press(targetdata)	_RCD_press((rt_uint8_t*)(&targetdata),sizeof(targetdata))
-//extern rt_uint8_t _RCD_press(rt_uint8_t *targetdata,rt_uint8_t length);
-
-////判断某个键是否产生松开动作（！只能判断按键和鼠标数据，若传入其他参数会产生未知结果！）
-//#define RCD_loosen(targetdata)	_RCD_loosen((rt_uint8_t*)(&targetdata),sizeof(targetdata))
-//extern rt_uint8_t _RCD_loosen(rt_uint8_t *targetdata,rt_uint8_t length);
-
+extern switch_action_e Change_from_middle(switch_action_e sx);
+extern switch_action_e Change_to_middle(switch_action_e sx);
 #endif
+
--- public/drv_strike.h
+++ public/drv_strike.h
@@ -6,38 +6,65 @@
 #include "drv_remote.h"
 #include "drv_canthread.h"
 
-#define HEAT_PERIOD											100				/*热量控制周期 100ms*/
+#define LOCAL_HEAT_ENABLE		0					            /*本地热量使能*/
+
+
+#define HEAT_PERIOD		        100								/*热量检测周期，单位ms*/
 /*发弹模式*/
-#define STRICK_NOLIMITE	    0x01								        /*全自动*/
-#define	STRICK_SINGLE       0x02										/*单发*/
-#define	STRICK_TRIPLE       0x04										/*三连发*/
+#define STRICK_NOLIMITE			0x01							/*无限制开火*/
+#define	STRICK_SINGLE           0x02							/*点射*/
+#define	STRICK_TRIPLE           0x04							/*三连发*/
 
-#define	STRICK_LOWSPEED 	0x08										/*低速*/
-#define	STRICK_MIDDLESPEED	0x10										/*中速*/
-#define	STRICK_HIGHTSPEED   0x20										/*高速*/
-
-#define STRICK_STUCK				0x01								/*卡弹*/
-#define STRICK_STOP					0x02								/*停止发弹*/
-
-
-/*热量模块*/
+#define	STRICK_LOWSPEED 		0x08							/*低射速*/
+#define	STRICK_MIDDLESPEED	    0x10							/*中射速*/
+#define	STRICK_HIGHTSPEED       0x20							/*高射速*/
+/*机构状态*/
+#define STRICK_STUCK		    0x01							/*卡弹*/
+#define STRICK_STOP				0x02							/*禁止发射*/
+/*热量控制结构体*/
 typedef struct _Heatctrl_t
 {
-	rt_int32_t 					now;							/*当前热量*/
+	rt_int32_t 					now;							/*现在剩余热量*/
 	rt_uint32_t 				max;							/*热量上限*/
-	rt_uint8_t 					rate;							/*剩余热量百分比*/
+	rt_uint8_t 					rate;							/*当前剩余热量百分比*/
+	struct _Heatctrl_t *next;							        /*下个热量控制块*/
+
+	/*如果使能本地热量计算*/
+	#if LOCAL_HEAT_ENABLE
+	rt_uint8_t 				  cool;							    /*枪口冷却值*/
+	rt_uint8_t    			buff;							    /*枪口冷却buff，即枪口冷却翻几倍*/
+	#endif
 }Heatctrl_t;
 
 
-/*发射模块*/
+/*发射机构结构体*/
 typedef struct
 {
-	rt_uint16_t   speed;										/*发弹速度 建议m/s*/
-	rt_uint8_t    mode;											/*发弹模式*/
-	rt_uint8_t	  status;										/*发弹状态*/
-	Heatctrl_t 	  heat;											/*热量模块*/
+	rt_int16_t    speed;										    /*摩擦轮速度*/
+	rt_uint8_t    mode;											/*射击模式，如点射，3弹连发（之后需要改成枚举）*/
+	rt_uint8_t	  status;										/*状态，如速度环卡弹，位置卡弹*/
+	Heatctrl_t 	  heat;										/*枪口热量控制块*/
 }Strike_t;
+
+extern Strike_t gun1;
+extern Motor_t m_rub[2];
+extern Motor_t m_launch;
+/*热量控制函数，由用户根据实际编写*/
+void heat_control(Heatctrl_t *p_temp);
+/*热量参数设置*/
+void heatctrl_init(Heatctrl_t *heat, rt_uint32_t max);
+/*开始热量控制*/
+void heatctrl_start(void);
 void motor_servo_set(uint16_t duty);
-void Gun_mode_set(Strike_t *strike,rt_base_t mode);
-static void Gun_speed_set(Refdata_t *Refdata,Strike_t *strike,rt_base_t mode);
+void Gun_speed_set(Strike_t *strike, rt_int16_t speed);
+void Gun_mode_set(Strike_t *strike, rt_base_t mode);
+/*发射机构初始化*/
+void strike_init(Strike_t *gun, rt_uint32_t max);
+void strike_start(void);
+/*对于多枪管，需要重新再定义一个相同作用函数，待修改*/
+/*卡弹判定*/
+void strike_stuck(Motor_t *motor, Strike_t *gun);
+/*对于多枪管，需要重新再定义一个相同作用函数，待修改*/
+/*设置模式并开火*/
+void strike_fire(Motor_t *motor, Strike_t *gun, rt_uint8_t if_fire);
 #endif
