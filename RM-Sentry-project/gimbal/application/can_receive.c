78ef24f0ffe9b61a1eea31a775b1a38d18c19d8c
yi_lu <1784570878@qq.com>
2020-08-20 13:32:31 +0800

    add sentry

---
+++ RM-Sentry-project/gimbal/application/can_receive.c
@@ -0,0 +1,44 @@
+#include "drv_canthread.h"
+#include "robodata.h"
+#include "drv_chassis.h"
+#include "drv_gyro.h"
+
+void can1_rec(struct rt_can_msg *msg)
+{
+    switch(msg->id)
+    {
+        //读取底盘数据
+        case LEFT_FRONT:
+        case RIGHT_FRONT:
+        case LEFT_BACK:
+        case RIGHT_BACK:
+            refresh_chassis_motor_data(msg);
+            return;
+
+        //读取陀螺仪数据
+        case GYRO_ANGLE_ID:
+            gyro_read_angle(msg);
+            return;
+        case GYRO_SPEED_ID:
+            gyro_read_speed(msg);
+            return;
+        
+        //默认return
+        default:
+            return;
+    }
+}
+
+void can2_rec(struct rt_can_msg *msg)
+{
+    switch(msg->id)
+    {
+        case YAW_ID:
+        case PITCH_ID:
+            refresh_gimbal_motor_data(msg);
+            return;
+        case CHASSIS_CTL:
+
+            return;
+    }
+}
---
+++ RM-Sentry-project/gimbal/rtconfig.h
@@ -0,0 +1,249 @@
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread Configuration */
+
+/* RT-Thread Kernel */
+
+#define RT_NAME_MAX 8
+#define RT_ALIGN_SIZE 4
+#define RT_THREAD_PRIORITY_32
+#define RT_THREAD_PRIORITY_MAX 32
+#define RT_TICK_PER_SECOND 100
+#define RT_USING_OVERFLOW_CHECK
+#define RT_USING_HOOK
+#define RT_USING_IDLE_HOOK
+#define RT_IDEL_HOOK_LIST_SIZE 4
+#define IDLE_THREAD_STACK_SIZE 256
+#define RT_USING_TIMER_SOFT
+#define RT_TIMER_THREAD_PRIO 4
+#define RT_TIMER_THREAD_STACK_SIZE 512
+#define RT_DEBUG
+
+/* Inter-Thread communication */
+
+#define RT_USING_SEMAPHORE
+#define RT_USING_MUTEX
+#define RT_USING_EVENT
+#define RT_USING_MAILBOX
+#define RT_USING_MESSAGEQUEUE
+
+/* Memory Management */
+
+#define RT_USING_MEMPOOL
+#define RT_USING_SMALL_MEM
+#define RT_USING_HEAP
+
+/* Kernel Device Object */
+
+#define RT_USING_DEVICE
+#define RT_USING_CONSOLE
+#define RT_CONSOLEBUF_SIZE 128
+#define RT_CONSOLE_DEVICE_NAME "uart"
+#define RT_VER_NUM 0x30103
+#define ARCH_ARM
+#define ARCH_ARM_CORTEX_M
+#define ARCH_ARM_CORTEX_M4
+
+/* RT-Thread Components */
+
+#define RT_USING_COMPONENTS_INIT
+#define RT_USING_USER_MAIN
+#define RT_MAIN_THREAD_STACK_SIZE 2048
+#define RT_MAIN_THREAD_PRIORITY 10
+
+/* C++ features */
+
+
+/* Command shell */
+
+#define RT_USING_FINSH
+#define FINSH_THREAD_NAME "tshell"
+#define FINSH_USING_HISTORY
+#define FINSH_HISTORY_LINES 5
+#define FINSH_USING_SYMTAB
+#define FINSH_USING_DESCRIPTION
+#define FINSH_THREAD_PRIORITY 20
+#define FINSH_THREAD_STACK_SIZE 4096
+#define FINSH_CMD_SIZE 80
+#define FINSH_USING_MSH
+#define FINSH_USING_MSH_DEFAULT
+#define FINSH_ARG_MAX 10
+
+/* Device virtual file system */
+
+#define RT_USING_DFS
+#define DFS_USING_WORKDIR
+#define DFS_FILESYSTEMS_MAX 2
+#define DFS_FILESYSTEM_TYPES_MAX 2
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_DEVFS
+
+/* Device Drivers */
+
+#define RT_USING_DEVICE_IPC
+#define RT_PIPE_BUFSZ 512
+#define RT_USING_SERIAL
+#define RT_SERIAL_USING_DMA
+#define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_CAN
+#define RT_CAN_USING_HDR
+#define RT_USING_PIN
+#define RT_USING_PWM
+#define RT_USING_WDT
+
+/* Using WiFi */
+
+
+/* Using USB */
+
+
+/* POSIX layer and C standard library */
+
+#define RT_USING_LIBC
+#define RT_USING_POSIX
+
+/* Network */
+
+/* Socket abstraction layer */
+
+
+/* Network interface device */
+
+
+/* light weight TCP/IP stack */
+
+
+/* Modbus master and slave stack */
+
+
+/* AT commands */
+
+
+/* VBUS(Virtual Software BUS) */
+
+
+/* Utilities */
+
+
+/* RT-Thread online packages */
+
+/* IoT - internet of things */
+
+
+/* Wi-Fi */
+
+/* Marvell WiFi */
+
+
+/* Wiced WiFi */
+
+
+/* IoT Cloud */
+
+
+/* security packages */
+
+
+/* language packages */
+
+
+/* multimedia packages */
+
+
+/* tools packages */
+
+#define PKG_USING_SYSTEMVIEW
+#define PKG_SYSVIEW_APP_NAME "RT-Thread Trace"
+#define PKG_SYSVIEW_DEVICE_NAME "Cortex-M4"
+#define PKG_SYSVIEW_TIMESTAMP_FREQ 0
+#define PKG_SYSVIEW_CPU_FREQ 0
+#define PKG_SYSVIEW_RAM_BASE 0x20000000
+#define PKG_SYSVIEW_EVENTID_OFFSET 32
+#define PKG_SYSVIEW_USE_CYCCNT_TIMESTAMP
+#define PKG_SYSVIEW_SYSDESC0 "I#15=SysTick"
+#define PKG_SYSVIEW_SYSDESC1 ""
+#define PKG_SYSVIEW_SYSDESC2 ""
+
+/* Segger RTT configuration */
+
+#define PKG_SEGGER_RTT_MAX_NUM_UP_BUFFERS 3
+#define PKG_SEGGER_RTT_MAX_NUM_DOWN_BUFFERS 3
+#define PKG_SEGGER_RTT_BUFFER_SIZE_UP 1024
+#define PKG_SEGGER_RTT_BUFFER_SIZE_DOWN 16
+#define PKG_SEGGER_RTT_PRINTF_BUFFER_SIZE 64
+#define PKG_SEGGER_RTT_AS_SERIAL_DEVICE
+#define PKG_SERIAL_DEVICE_NAME "segger"
+#define PKG_SEGGER_RTT_MODE_ENABLE_NO_BLOCK_SKIP
+#define PKG_SEGGER_RTT_MAX_INTERRUPT_PRIORITY 0x20
+
+/* SystemView buffer configuration */
+
+#define PKG_SEGGER_SYSVIEW_RTT_BUFFER_SIZE 1024
+#define PKG_SEGGER_SYSVIEW_RTT_CHANNEL 1
+#define PKG_SEGGER_SYSVIEW_USE_STATIC_BUFFER
+
+/* SystemView Id configuration */
+
+#define PKG_SEGGER_SYSVIEW_ID_BASE 0x10000000
+#define PKG_SEGGER_SYSVIEW_ID_SHIFT 2
+#define PKG_USING_SYSTEMVIEW_LATEST_VERSION
+
+/* system packages */
+
+
+/* peripheral libraries and drivers */
+
+
+/* miscellaneous packages */
+
+
+/* samples: kernel and components samples */
+
+#define SOC_FAMILY_STM32
+#define SOC_SERIES_STM32F4
+
+/* Hardware Drivers Config */
+
+#define SOC_STM32F427VI
+
+/* Onboard Peripheral Drivers */
+
+#define CORE_USING_BUZZER
+#define CORE_USING_RGB_LIGHT
+
+/* On-chip Peripheral Drivers */
+
+#define BSP_USING_GPIO
+#define BSP_USING_UART
+#define BSP_USING_UART1
+#define BSP_UART1_RX_USING_DMA
+#define BSP_USING_UART6
+#define BSP_UART6_RX_USING_DMA
+#define BSP_USING_UART8
+#define BSP_USING_PWM
+#define BSP_USING_PWM2
+#define BSP_USING_PWM2_CH3
+#define BSP_USING_PWM5
+#define BSP_USING_PWM5_CH3
+#define BSP_USING_PWM9
+#define BSP_USING_PWM9_CH1
+#define BSP_USING_PWM9_CH2
+#define BSP_USING_CAN
+#define BSP_USING_CAN1
+#define BSP_USING_CAN2
+
+/* Board extended module Drivers */
+
+/* Thread Config */
+
+#define THREAD_GIMBAL_CONTROL
+#define THREAD_STRIKE_CONTRAL
+#define THREAD_CAN_RECEIVE
+#define THREAD_REMOTE
+#define MOTOR_CONTROL
+#define PID_CALCULATE
+#define USING_GYRO
+
+#endif
